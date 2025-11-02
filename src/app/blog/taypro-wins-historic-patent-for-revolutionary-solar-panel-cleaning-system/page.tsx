5dc8000ec50cdd27d5e37dbd3f82460420d47e63
Yogesh Kudale <mryogeshk4@gmail.com>
2025-11-03 01:53:19 +0530

    Fix duplicate fillRule attribute in patent blog post

--- src/app/blog/taypro-wins-historic-patent-for-revolutionary-solar-panel-cleaning-system/page.tsx
+++ src/app/blog/taypro-wins-historic-patent-for-revolutionary-solar-panel-cleaning-system/page.tsx
@@ -93,17 +93,17 @@ export default function BlogPost() {
               </span>
 
               <span className="flex items-center gap-2">
-                <svg
-                  className="w-4 h-4"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-                    fillRule="evenodd"
-                  />
-                </svg>
+                    <svg
+                      className="w-4 h-4"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
                 Taypro Team
               </span>
             </div>
